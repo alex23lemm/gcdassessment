15829f0998eec815b165ae1745aa7b8700b5a52f
Alex Lemm <alex23lemm@gmail.com>
2014-04-27 18:43:53 +0200

    * Optimize regex * Add code for re-labeling column activity headers * Replace data.table summary call with ddply call

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 # Load libraries ---------------------------------------------------------------
 
-library(data.table)
 library(plyr)
 
 
@@ -35,27 +34,26 @@ test_set <- cbind(test_subjects, test_activities, test_set)
 merged_set <- rbind(training_set, test_set)
 colnames(merged_set) <- c('subject', 'activity', feature_names)
 
-# 2. Extract only the measurements on the mean and standard deviation for each 
-#    measurement 
-focus_features <- grepl('(.*?)-(mean|std)(.*?)', feature_names)
+# 2. Extract only the measurements on the mean and standard deviation
+focus_features <- grepl('(.*)-(mean|std)\\(\\)(.*)', feature_names)
 merged_set <- merged_set[, c(TRUE, TRUE, focus_features)]
 
-# 3./4. Uses descriptive activity names to name the activities in the data set
-#       Appropriately labels the data set with descriptive activity names
+# 3. Uses descriptive activity names to name the activities in the data set
 merged_set$activity <- mapvalues(merged_set$activity , c(1, 2, 3, 4, 5, 6),
                                  c('walking', 'walking upstairs', 
                                    'walking downstairs', 'sitting', 'standing',
                                    'laying'))
 
-# 5. Create a second, independent tidy data set with the average of each variable
-#    for each activity and each subject
-merged_set <- as.data.table(merged_set)
-merged_set <- merged_set[, lapply(.SD, mean), by = list(activity, subject) ]
-write.table(merged_set, file = "./data/tidy_samsung_data.txt", quote = FALSE,
+# 4. Appropriately labels the data set with descriptive activity names
+colnames(merged_set) <- gsub('[-()]', '', colnames(merged_set))
+colnames(merged_set) <- sub('mean', 'Mean', colnames(merged_set))
+colnames(merged_set) <- sub('std', 'Std', colnames(merged_set))
+
+# 5. Create a second, independent tidy data set with the average of each 
+#    variable for each activity and each subject
+tidy_set <- ddply(merged_set, c('activity', 'subject'), numcolwise(mean))
+write.table(tidy_set, file = "./data/tidy_samsung_data.txt", quote = FALSE,
             row.names = FALSE, sep='\t')
-
-
-
 
 
 
